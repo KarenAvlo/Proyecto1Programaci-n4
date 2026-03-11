378962b71843619383418135d86e2580940bb6b5
KarenAvlo <karen.aviles.lopez@est.una.ac.cr>
2026-03-10 21:18:58 -0600

    Estructura inicial del proyecto Spring Boot

---
+++ src/main/java/com/example/proyecto1programacion4/Proyecto1Programacion4Application.java
@@ -0,0 +1,13 @@
+package com.example.proyecto1programacion4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto1Programacion4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Proyecto1Programacion4Application.class, args);
+    }
+
+}
